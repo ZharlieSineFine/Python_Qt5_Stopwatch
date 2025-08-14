94c9dd6148ec589de8c7eeda9a33f26bb0816f9d
Zhao Changlin <huskydawg@foxmail.com>
2025-08-14 09:42:40 +0800

    Set up time label, buttons, and a timer

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from PyQt5.QtCore import QTimer, QTime, Qt
 class Stopwatch(QWidget):
     def __init__(self):
         super().__init__()
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label = QLabel("00:00:00.00", self)
+        self.start_button = QPushButton("Start", self)
+        self.stop_button = QPushButton("Stop", self)
+        self.reset_button = QPushButton("Reset", self)
+        self.timer = QTimer(self)
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
 
 
 
